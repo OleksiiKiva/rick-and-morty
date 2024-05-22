6db1c49fb6bdb35baae536fd617e0e2479e62a52
Oleksii Kiva <oleksii.kiva@gmail.com>
2024-05-22 21:25:10 +0300

    feat: add scrapper functionality to Rick&Morty service

---
+++ characters/scraper.py
@@ -0,0 +1,38 @@
+import requests
+from django.conf import settings
+
+from characters.models import Character
+
+
+def scrape_characters() -> list[Character]:
+    next_url_to_scrape = settings.RICK_AND_MORTY_API_CHARACTERS_URL
+
+    characters = []
+    while next_url_to_scrape is not None:
+        characters_response = requests.get(next_url_to_scrape).json()
+
+        for character_dict in characters_response.get("results"):
+            characters.append(
+                Character(
+                    api_id=character_dict.get("id"),
+                    name=character_dict.get("name"),
+                    status=character_dict.get("status"),
+                    species=character_dict.get("species"),
+                    gender=character_dict.get("gender"),
+                    image=character_dict.get("image"),
+                )
+            )
+
+        next_url_to_scrape = characters_response.get("info", {}).get("next", {})
+
+    return characters
+
+
+def save_characters(characters: list[Character]) -> None:
+    for character in characters:
+        character.save()
+
+
+def sync_characters_with_api() -> None:
+    characters = scrape_characters()
+    save_characters(characters)
